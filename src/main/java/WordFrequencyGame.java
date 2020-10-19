8e42194b315ad87a6451bfcfe30e0dbd8ddde43d
BARON MANZANO <MANZABA@OOCL.COM>
2020-10-19 15:41:28 +0800

    [word-frequency][baron] refactor: renamed arr to words

--- src/main/java/WordFrequencyGame.java
+++ src/main/java/WordFrequencyGame.java
@@ -21,10 +21,10 @@ public class WordFrequencyGame {
             try {
 
                 //split the input string with 1 to n pieces of spaces
-                String[] arr = inputStr.split(WHITESPACE_REGEX);
+                String[] words = inputStr.split(WHITESPACE_REGEX);
 
                 List<Input> inputList = new ArrayList<>();
-                for (String s : arr) {
+                for (String s : words) {
                     Input input = new Input(s, 1);
                     inputList.add(input);
                 }
